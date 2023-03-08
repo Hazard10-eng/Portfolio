3efcf632cd829b81406f71efab113ba97236faa4
Mahabub <mahabubx7@gmail.com>
2023-03-08 20:01:06 +0600

    :rocket: made-form-validation

--- index.js
+++ index.js
@@ -160,3 +160,32 @@ projects.map((project) => {
   });
 });
 
+
+// validation for contact form
+const submit = document.getElementById('submitForm');
+const form = document.getElementById('contact-form');
+
+function checkEmail(str) {
+  return typeof(str) === 'string' && str === str.toLowerCase();
+}
+
+let formData = {};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formData = Object.assign({
+    name: event.currentTarget.name.value || '',
+    email: event.currentTarget.email.value || '',
+    feedback: event.currentTarget.feedback.value || '',
+  });
+
+  if (checkEmail(formData['email'])) {
+    form.submit();
+  } else {
+    // console.log("Error!", formData);
+    const error = document.createElement('p');
+    error.innerText = "Invalid Email Address!";
+    error.classList.add('error');
+    form.appendChild(error);
+  }
+});
