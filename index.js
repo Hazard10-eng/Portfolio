f45e725241242e55f7e0470cb8c8905253d06487
unknown <sebagajeanaime0@gmail.com>
2023-03-08 12:53:15 +0100

    Add html file

--- index.js
+++ index.js
@@ -20,3 +20,82 @@ function interactions() {
 }
 
 interactions();
+/* eslint-disable array-callback-return */
+// store of projects
+const cards=[
+    {
+    name:'Tonic',
+    image:'./image/Snapshoot Portfolio.svg',
+    description:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technology:[{
+        name1:'html',
+        name2:'css',
+        name3:'Javascript',
+
+    }]
+},
+   { 
+    name:'Tonic',
+    image:'./image/Snapshoot Portfolio.svg',
+    description:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technology:[{
+        name1:'html',
+        name2:'css',
+        name3:'Javascript',
+
+    }] 
+  },
+    {name:'Tonic',
+    image:'./image/Snapshoot Portfolio.svg',
+    description:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technology:[{
+        name1:'html',
+        name2:'css',
+        name3:'Javascript',
+
+    }]
+  },
+     {
+        name:'Tonic',
+    image:'./image/Snapshoot Portfolio.svg',
+    description:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technology:[{
+        name1:'html',
+        name2:'css',
+        name3:'Javascript',
+
+    }]
+},
+    {
+    name:'Tonic',
+    image:'./image/Snapshoot Portfolio.svg',
+    description:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technology:[{
+        name1:'html',
+        name2:'css',
+        name3:'Javascript',
+
+    }]
+}, 
+    {
+    name:'Tonic',
+    image:'./image/Snapshoot Portfolio.svg',
+    description:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technology:[{
+        name1:'html',
+        name2:'css',
+        name3:'Javascript',
+
+    }]
+}]
+
+
+ let div = document.createElement('div')
+ div.className='project-details';
+ document.body.appendChild(div);
+ let h2=document.createElement('h2');
+ h2.textContent='add h2 element to the div';
+ div.appendChild(h2);
+ const card=document.querySelector("carte")
+
+ 
