35f2e83d753d487c9a2fb85f38b10ecc3c408de4
Mahabub <mahabubx7@gmail.com>
2023-03-08 14:42:34 +0600

    :hammer: dynamic-html-js

--- index.js
+++ index.js
@@ -20,3 +20,72 @@ function interactions() {
 }
 
 interactions();
+
+// portfolio-works section render dynamically
+const projects = [
+  {
+    name: "Tonic",
+    bullets: {
+      client: 'Canopy',
+      tech: 'Back End Dev',
+      year: 2015
+    },
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    preview: "#",
+    code: "#",
+  },
+  {
+    name: "Multipost stories",
+    bullets: {
+      client: 'Canopy',
+      tech: 'Back End Dev',
+      year: 2015
+    },
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    preview: "#",
+    code: "#",
+  },
+  {
+    name: "Tonic",
+    bullets: {
+      client: 'Canopy',
+      tech: 'Back End Dev',
+      year: 2015
+    },
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    preview: "#",
+    code: "#",
+  },
+];
+const workContainer = document.getElementById("work");
+projects.map((project) => {
+  const container = document.createElement('div');
+  container.classList.add('project');
+  const details = document.createElement('div');
+  details.innerHTML = `
+  <div class="project-details">
+        <div class="details">
+          <h2 class="project-title">${project.name}</h2>
+          <nav class="project-nav">
+            <h4 class="project-client">${project.bullets.client}</h4>
+
+            <img src="./image/Counter.svg" alt="counter" />
+            <span class="project-lang">${project.bullets.tech}</span>
+            <img src="./image/Counter.svg" alt="counter" />
+
+            <span class="year">${project.bullets.year}</span>
+          </nav>
+          <p class="project-description">${project.description}}</p>
+          <ul class="pro-tool">
+            <li class="tool">html</li>
+            <li class="tool">css</li>
+            <li class="tool">javascript</li>
+          </ul>
+          <button class="more">see project</button>
+        </div>
+      </div>
+  `;
+  container.appendChild(details);
+  workContainer.appendChild(container);
+});
+
